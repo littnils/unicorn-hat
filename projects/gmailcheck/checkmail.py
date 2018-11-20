df0e9473fab3e73c970c3c2f35f7e212a2135cda
Nils Littmann <nlittmann@theorchard.com>
2018-11-20 17:55:28 +0100

    multi threading and error handling

--- projects/gmailcheck/checkmail.py
+++ projects/gmailcheck/checkmail.py
@@ -2,11 +2,11 @@
 
 import imaplib
 
-from os import getenv
+import threading
 
 from random import randint
+
 import time
-from time import sleep
 
 import unicornhat as unicorn
 
@@ -20,18 +20,47 @@ width, height = unicorn.get_shape()
 
 
 MAIL_REFRESH_INTERVAL = 10
-BLINK_REFRESH_INTERVAL = 0.1
+BLINK_REFRESH_INTERVAL = 1
+
+MIN_RGB = 40
+MAX_RGB = 255
 
 SMTP_SERVER = "imap.gmail.com"
 FROM_EMAIL = ""
 FROM_PWD = ""
 
+mails = 0
+
 def read_email_from_gmail():
-    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-    mail.login(FROM_EMAIL, FROM_PWD)
-    mail.select('inbox')
-    unread_mails = len(mail.search(None, 'UnSeen')[1][0].split())
-    return unread_mails
+    global mails
+    while True:
+        print("checking gmail...")
+        for attempt in range(20):
+            try:
+                mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+                mail.login(FROM_EMAIL, FROM_PWD)
+                mail.select('inbox')
+                unread_mails = len(mail.search(None, 'UnSeen')[1][0].split())
+            except:
+                print("attempt " + str(attempt) + " failed, trying again...")
+            else:
+                break
+        print(unread_mails)
+        mails = unread_mails
+        time.sleep(MAIL_REFRESH_INTERVAL)
+
+
+def bootup():
+    for y in range(height):
+        for x in range(width):
+            r = randint(MIN_RGB, MAX_RGB)
+            g = randint(MIN_RGB, MAX_RGB)
+            b = randint(MIN_RGB, MAX_RGB)
+            unicorn.set_pixel(x, y, r, g, b)
+            unicorn.show()
+            time.sleep(0.1)
+    time.sleep(2)
+    clean_screen()
 
 
 def clean_screen():
@@ -41,23 +70,29 @@ def clean_screen():
             unicorn.show()
 
 
-def random_blinks(blink_amount):
-    for i in range(blink_amount):
-        r = randint(30, 255)
-        g = randint(30, 255)
-        b = randint(30, 255)
-        y = randint(0, height)
-        x = randint(0, width)
-        unicorn.set_pixel(x, y, r, g, b)
-        unicorn.show()
-    clean_screen()
+def random_blinks():
+    global mails
+    print("starting random blinks")
+    while True:
+        print("mails is " + str(mails))
+        for i in range(mails):
+            r = randint(MIN_RGB, MAX_RGB)
+            g = randint(MIN_RGB, MAX_RGB)
+            b = randint(MIN_RGB, MAX_RGB)
+            y = randint(0, height)
+            x = randint(0, width)
+            unicorn.set_pixel(x, y, r, g, b)
+            unicorn.show()
+        clean_screen()
+        time.sleep(BLINK_REFRESH_INTERVAL)
 
 
 if __name__ == "__main__":
-    while True:
-        unreadMails = read_email_from_gmail()
-        now = int(time.time())
-        stop_at = now + MAIL_REFRESH_INTERVAL
-        while int(time.time()) < stop_at:
-            random_blinks(unreadMails)
-            sleep(BLINK_REFRESH_INTERVAL)
+    bootup()
+    t1 = threading.Thread(target = random_blinks)
+    t2 = threading.Thread(target = read_email_from_gmail)
+
+    t1.start()
+    t2.start()
+
+
