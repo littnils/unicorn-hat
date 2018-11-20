1d2c3679d48c95fa24c657df75e93585d6965cda
Nils Littmann <n.littmann@finetunes.net>
2018-11-20 13:34:54 +0100

    added blinks

--- projects/gmailcheck/checkmail.py
+++ projects/gmailcheck/checkmail.py
@@ -4,20 +4,56 @@ import imaplib
 
 from os import getenv
 
-# -------------------------------------------------
-#
-# Utility to read email from Gmail Using Python
-#
-# ------------------------------------------------
+from random import randint
+import time
+from time import sleep
+
+import unicornhat as unicorn
+
+
+# setup the unicorn hat
+unicorn.set_layout(unicorn.AUTO)
+unicorn.brightness(0.5)
+
+# get the width and height of the hardware
+width, height = unicorn.get_shape()
+
+
+MAIL_REFRESH_INTERVAL = 10
+BLINK_REFRESH_INTERVAL = 0.1
+
 
 def read_email_from_gmail():
-
     mail = imaplib.IMAP4_SSL(getenv('SMTP_SERVER'))
     mail.login(getenv('FROM_EMAIL'), getenv('FROM_PWD'))
     mail.select('inbox')
-
     unread_mails = len(mail.search(None, 'UnSeen')[1][0].split())
+    return unread_mails
 
-    print(unread_mails)
 
-read_email_from_gmail()
+def clean_screen():
+    for y in range(height):
+        for x in range(width):
+            unicorn.set_pixel(x, y, 0, 0, 0)
+            unicorn.show()
+
+
+def random_blinks(blink_amount):
+    for i in range(blink_amount):
+        r = randint(30, 255)
+        g = randint(30, 255)
+        b = randint(30, 255)
+        for y in range(height):
+            for x in range(width):
+                unicorn.set_pixel(x, y, r, g, b)
+                unicorn.show()
+
+
+if __name__ == "__main__":
+    while True:
+        unreadMails = read_email_from_gmail()
+        now = int(time.time())
+        stop_at = now + MAIL_REFRESH_INTERVAL
+        while int(time.time()) < stop_at:
+            random_blinks(unreadMails)
+            sleep(0.5)
