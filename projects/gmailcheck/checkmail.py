9dd48ee3fb44380bd23c52fa16fff67db13ffa59
Nils Littmann <n.littmann@finetunes.net>
2018-11-20 13:13:14 +0100

    super simplified

--- projects/gmailcheck/checkmail.py
+++ projects/gmailcheck/checkmail.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-import smtplib
-import time
 import imaplib
-import email
+
 from os import getenv
 
 # -------------------------------------------------
@@ -13,31 +11,13 @@ from os import getenv
 # ------------------------------------------------
 
 def read_email_from_gmail():
-    try:
-        mail = imaplib.IMAP4_SSL(getenv('SMTP_SERVER'))
-        mail.login(getenv('FROM_EMAIL'),getenv('FROM_PWD'))
-        mail.select('inbox')
 
-        type, data = mail.search(None, 'ALL')
-        mail_ids = data[0]
+    mail = imaplib.IMAP4_SSL(getenv('SMTP_SERVER'))
+    mail.login(getenv('FROM_EMAIL'), getenv('FROM_PWD'))
+    mail.select('inbox')
 
-        id_list = mail_ids.split()   
-        first_email_id = int(id_list[0])
-        latest_email_id = int(id_list[-1])
+    unread_mails = len(mail.search(None, 'UnSeen')[1][0].split())
 
-
-        for i in range(latest_email_id,first_email_id, -1):
-            typ, data = mail.fetch(i, '(RFC822)' )
-
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_string(response_part[1])
-                    email_subject = msg['subject']
-                    email_from = msg['from']
-                    print 'From : ' + email_from + '\n'
-                    print 'Subject : ' + email_subject + '\n'
-
-    except Exception, e:
-        print str(e)
+    print(unread_mails)
 
 read_email_from_gmail()
